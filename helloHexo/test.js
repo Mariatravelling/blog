a6e567c73e475dbcc0ffa435d55d3ce58ad63fda
Wang <zehui.wang@sap.com>
2019-06-12 16:49:39 +0200

    change the script

--- helloHexo/test.js
+++ helloHexo/test.js
@@ -1,11 +1,18 @@
 
-const port = process.env.PORT || 4000;
+// const port = process.env.PORT || 4000;
 
-// run hexo server 
-var exec = require('child_process').exec;
+// // run hexo server 
+// var exec = require('child_process').exec;
 
-var child = exec('./node_modules/.bin/hexo server -p' + port, function(error, stdout, stderr) {
-    console.log(stdout);
-}); 
+// var child = exec('./node_modules/.bin/hexo server -p' + port, function(error, stdout, stderr) {
+//     console.log(stdout);
+// }); 
 
 
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!  THIS IS A TEST!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
